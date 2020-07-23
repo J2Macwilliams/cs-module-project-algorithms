f3b95a2ebe72597249747d9d3459fb483e46cf36
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-07-23 14:57:03 -0700

    write better solution for sliding window

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -20,25 +20,28 @@ def sliding_window_max(nums, k):
     #  use some sort of iterator I guess
     # k must be smaller  or equal than nums
     while end <= len(nums): # O(n)
-        # k is a slice of nums
-        my_window = [ x for x in nums[start:end]] # O(n)
-        # max identifies the highest
-        my_max = max(my_window)
-        new_array.append(my_max)
-        # increment k and start
+        # window is a slice of nums 
+        new_array.append(max(nums[start:end]))
+        # increment start and end
         start += 1
         end += 1
     
     # return max items
     return new_array
+
 end_time = time.time()
 
-sliding_window_max([1, 3, -1, -3, 5, 3, 6, 7], 3) # runtime: 1.4066696166992188e-05 seconds
+sliding_window_max([1, 3, -1, -3, 5, 3, 6, 7], 3) 
 
 print(f"runtime: {end_time - start_time} seconds")
 
 #First Pass
-# Runtime complexity: o(n^2), however the second (n) is based off the size of k which dramatically shortens the time.
+# runtime: 2.1457672119140625e-06 seconds
+# Runtime complexity: O(n^2), however the second (n) is based off the size of k which dramatically shortens the time.
+
+# Better Solution
+# runtime: 1.1920928955078125e-06 seconds
+# Runtime Complexity: O(n)
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation 
