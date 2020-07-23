277b9fd12e7a9f9d3ad50b3014b8561b65e1b154
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-07-23 07:06:08 -0700

    work first pass at eating cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -3,9 +3,29 @@ Input: an integer
 Returns: an integer
 '''
 def eating_cookies(n):
-    # Your code here
+    # Plan naive
+    
+    # there are only a limited amount of cookies
+    # problem looks similar to fibonacci
+    # except with three options
+    # try Tribonacci sequence
+    # set up base case
+    # if cookie is greater than amount of cookies:
+    if n == 0:
+        return 0
+    # if cookie is equal to one
+    elif n == 1:
+        return 1
+    # if cookie is equal to two
+    elif n == 2:
+        return 1
+    # if cookie is equal to three
+    elif n ==3:
+        return 1
+    # create recursive call
+    else:
+        eating_cookies(n - 1) + eating_cookies(n - 2) + eating_cookies(n - 3)
 
-    pass
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
